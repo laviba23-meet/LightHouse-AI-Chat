e012b7a1fa9c5bf67a0acddde2bde34316586739
laviba23-meet <109899660+laviba23-meet@users.noreply.github.com>
2023-07-24 20:05:02 +0300

    co

--- AI.py
+++ AI.py
@@ -8,7 +8,7 @@ import openai
 
 
 
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = "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"
 openai.api_key = os.environ["OPENAI_API_KEY"]
 # define prompt helper
 max_input_size = 4096
